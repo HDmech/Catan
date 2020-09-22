79fbafae078b0da4fe24840bab78e4dbd182fbe9
Hans Dijkstra <hdaccntspm@outlook.com>
2020-09-22 20:01:02 +0200

    added immutabilty to Board.py

--- Model/Board.py
+++ Model/Board.py
@@ -1,25 +1,24 @@
 from random import shuffle
 
+
 class Board:
     def __init__(self):
-        numberFiches = [x for i in range(2,13) for x in [i]*3]
+        numberFiches = [x for i in range(2, 13) for x in [i]*3]
         shuffle(numberFiches)
         self.tileList = [self.Tile(numberFiches[i]) for i in range(19)]
-        
 
     def __str__(self):
-        s="Board:"
-        s+=' '.join(str(tile) for tile in self.tileList)
-        
+        s = "Board:"
+        s += ' '.join(str(tile) for tile in self.tileList)
+
         return s
 
-    
+    def __setattr__(self, key, value): pass
+    def __getattr__(self, key): pass
 
     class Tile:
-        def __init__(self,value):
+        def __init__(self, value):
             self.value = value
-
 
         def __str__(self):
             return f"Tile: {self.value}"
-
